86822bdb06f728ca32a09234bf0440dfd9a5a78f
GANABA-25 <nathanielowusu01@gmail.com>
2026-09-16 07:40:40 -0700

    completed landing page structure

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} text-base`}>{children}</body>
+      <body className={`${poppins.className} text-base`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- frontend/components/button.tsx
+++ frontend/components/button.tsx
@@ -9,7 +9,7 @@ export default function Button({ children }: ButtonProps) {
     <button
       className="
         flex items-center gap-2 rounded-xl bg-primary p-3 px-6 font-bold capitalize text-black transition-all duration-300 ease-out hover:-translate-y-0.5 hover:scale-[1.02] hover:bg-primary-light hover:shadow-[0_8px_25px_rgba(248,191,75,0.25)] active:translate-y-0
-        active:scale-[0.98] focus:outline-none focus:ring-2 focus:ring-primary/40"
+        active:scale-[0.98] focus:outline-none focus:ring-2 focus:ring-primary/40 cursor-pointer shadow-sm"
     >
       {children}
     </button>
